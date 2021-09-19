e9b9393a8e4941254bbb5af629d2fddff95aff66
Naksen <90452587+Naksen@users.noreply.github.com>
2021-09-20 00:30:10 +0300

    Add files via upload

---
+++ OS_lab1/child_2.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    //cout << "Child2 is open\n";
+    
+    auto *in = new char[2];
+    in[0] = 0;
+    char c;
+    while ((c = getchar()) != EOF) {
+
+        in[0] += 1;
+        in[in[0]] = c;
+        in = (char *) realloc(in, (in[0] + 2) * sizeof(char));
+
+    }
+    in[in[0]] = '\0';
+
+    char *out = (char *) malloc(2 * sizeof(char));
+    out[0] = 0;
+
+    for (int i = 1; i < in[0] + 1; i++) {
+
+        if (in[i] == ' ' && in[i + 1] == ' ') {
+
+            i++;
+            continue;
+
+        }
+
+        out[0]++;
+        out[out[0]] = in[i];
+        out = (char *) realloc(out, (out[0] + 2) * sizeof(out));
+
+    }
+
+
+    for (int i = 1; i < out[0]; i++) {
+
+        cout << out[i];
+        
+    }
+
+    printf("\n");
+    fflush(stdout);
+    close(STDOUT_FILENO);
+    return 0;
+}
